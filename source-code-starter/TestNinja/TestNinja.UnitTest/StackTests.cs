710b9561795c66b57ace94764c36b4a46b290b29
Carlos Fuentes <char_basket@hotmail.com>
2022-11-17 09:44:17 +0100

    Finish Stack Test

--- source-code-starter/TestNinja/TestNinja.UnitTest/StackTests.cs
+++ source-code-starter/TestNinja/TestNinja.UnitTest/StackTests.cs
@@ -6,21 +6,91 @@ namespace TestNinja.UnitTest
     [TestFixture]
     public class StackTests
     {
-        [Test]
-        public void Push_ObjectIsNull_ThrowArgumentNullException()
+        [SetUp]
+        public void SetUp()
         {
-            var stack = new Stack<object>();
+            _stack = new Stack<string>();
+        }
 
-            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        private Stack<string> _stack;
+
+        [Test]
+        public void Push_ArgIsNull_ThrowArgumentNullException()
+        {
+            Assert.That(() => _stack.Push(null), Throws.ArgumentNullException);
         }
 
         [Test]
-        public void Push_ObjectIsNotNull_AddObjectToList()
+        public void Push_ValidArgument_AddObjectToTheStack()
         {
-            var stack = new Stack<int>();
-            stack.Push(1);
+            _stack.Push("a");
 
-            Assert.That(stack.Peek(), Is.EqualTo(1));
+            // We can not use this 
+            // Assert.That(_stack.Peek(), Is.EqualTo(1));
+            // We can not access the list because is private
+            // We only test the public API, so we assert only that the object has been pushed,
+            // but not if the object pushed is our object
+            Assert.That(_stack.Count, Is.EqualTo(1));
+        }
+
+        // We also need to test the Count property
+        // We test that when the _stack is empty, count = 0
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        {
+            Assert.That(_stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowsInvalidOperationException()
+        {
+            Assert.That(() => _stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_StackWithFewObjects_ReturnsObjectOnTop()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            var result = _stack.Pop();
+
+            Assert.That(result, Is.EqualTo("b"));
+        }
+
+        [Test]
+        public void Pop_StackWithFewObjects_RemovesObjectOnTop()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Pop();
+
+            Assert.That(_stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Peek_EmptyStack_ThrowsInvalidOperationException()
+        {
+            Assert.That(() => _stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_StackWithFewObjects_ReturnsObjectOnTop()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            var result = _stack.Peek();
+
+            Assert.That(result, Is.EqualTo("b"));
+        }
+
+        [Test]
+        public void Peek_StackWithFewObjects_DoesNotRemoveObjectOnTop()
+        {
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Peek();
+
+            Assert.That(_stack.Count, Is.EqualTo(2));
         }
     }
 }
